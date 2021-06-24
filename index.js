2e92c6e7a0a19ba61d5cb29c23953b47700fb8fe
Premananda Mohapatra <prem@mserv.in>
2021-06-24 18:31:18 +0530

    externalise cron exp

--- index.js
+++ index.js
@@ -28,48 +28,48 @@ const TELEGRAM_POST_URL = new URL('https://api.telegram.org/bot' + process.env.T
 TELEGRAM_POST_URL.searchParams.append("chat_id", process.env.TELEGRAM_CHAT_ID);
 TELEGRAM_POST_URL.searchParams.append("parse_mode", "HTML");
 
-// Schedule tasks to be run on the server.
-// “At every 15th minute past every hour from 4 through 10 UTC time on every day-of-week from Monday through Friday.”
-// */15 4-10 * * 1-5
-// cron.schedule('* * * * *', function() {
-//     //if (isDuringTradeingHours()) {
-//         console.log('running a task every minute between 10am and 3pm IST');
-//         fetch(TT_URL.href, options)
-//         .then(res => res.json())
-//         .then((strategies) => {
-//             console.log('Number of strategies = ', strategies.data.length);
-//             let telegram_msg = '<b>💰 PNL - </b>' + process.env.TRADE_TYPE + ' 💰' + '\r\n\r\n';
-//             let total_pnl = 0.0;
-//             strategies.data.forEach(deployment => {
-//                 //Only report the Live or exited PNL
-//                 if( deployment.status.search('Live-Entered') || deployment.status.search('Exited') ) {
-//                     let pnl = parseFloat(deployment.sum_of_pnl).toFixed(2);
-//                     total_pnl += parseFloat(deployment.sum_of_pnl);
+//Schedule tasks to be run on the server.
+//“At every 15th minute past every hour from 4 through 10 UTC time on every day-of-week from Monday through Friday.”
+//*/15 4-10 * * 1-5
+cron.schedule(process.env.CRONEXP, function() {
+    //if (isDuringTradeingHours()) {
+        console.log('running a task every minute between 10am and 3pm IST');
+        fetch(TT_URL.href, options)
+        .then(res => res.json())
+        .then((strategies) => {
+            console.log('Number of strategies = ', strategies.data.length);
+            let telegram_msg = '<b>💰 PNL - </b>' + process.env.TRADE_TYPE + ' 💰' + '\r\n\r\n';
+            let total_pnl = 0.0;
+            strategies.data.forEach(deployment => {
+                //Only report the Live or exited PNL
+                if( deployment.status.search('Live-Entered') || deployment.status.search('Exited') ) {
+                    let pnl = parseFloat(deployment.sum_of_pnl).toFixed(2);
+                    total_pnl += parseFloat(deployment.sum_of_pnl);
                     
-//                     //pruning only the strategy name by removing anything after //
-//                     let name = deployment.template.name.slice(0, deployment.template.name.indexOf('/'));
-//                     console.log(process.env.TRADE_TYPE + " >> " + name + " >> " + deployment.currency + pnl);
+                    //pruning only the strategy name by removing anything after //
+                    let name = deployment.template.name.slice(0, deployment.template.name.indexOf('/'));
+                    console.log(process.env.TRADE_TYPE + " >> " + name + " >> " + deployment.currency + pnl);
                     
-//                     //TODO : Google sheet integration to send each deployment PNL
+                    //TODO : Google sheet integration to send each deployment PNL
     
-//                     //Construct the message to be passed to telegram
-//                     telegram_msg += (pnl >= 0) ? PROFIT : LOSS;
-//                     telegram_msg += ' ' + name + ' ' + '<b>' + deployment.currency + pnl + '</b>' + '\r\n';
-//                 }
-//             });
-//             telegram_msg += '\r\n';
-//             telegram_msg += (total_pnl >= 0) ? PROFIT : LOSS;
-//             telegram_msg += " TOTAL PNL = " + '<b>' + parseFloat(total_pnl).toFixed(2) + '</b>';
+                    //Construct the message to be passed to telegram
+                    telegram_msg += (pnl >= 0) ? PROFIT : LOSS;
+                    telegram_msg += ' ' + name + ' ' + '<b>' + deployment.currency + pnl + '</b>' + '\r\n';
+                }
+            });
+            telegram_msg += '\r\n';
+            telegram_msg += (total_pnl >= 0) ? PROFIT : LOSS;
+            telegram_msg += " TOTAL PNL = " + '<b>' + parseFloat(total_pnl).toFixed(2) + '</b>';
 
-//             console.log(telegram_msg);
-//             //POST call to Telegram
-//             let full_telegram_url = new URL(TELEGRAM_POST_URL.toString());
-//             full_telegram_url.searchParams.append("text", telegram_msg);
-//             fetch(full_telegram_url, {method: 'POST'});
-//             }
-//         );
-//     //}
-// });
+            console.log(telegram_msg);
+            //POST call to Telegram
+            let full_telegram_url = new URL(TELEGRAM_POST_URL.toString());
+            full_telegram_url.searchParams.append("text", telegram_msg);
+            fetch(full_telegram_url, {method: 'POST'});
+            }
+        );
+    //}
+});
 
 // function isDuringTradeingHours() {
 //     //From env file
@@ -82,9 +82,9 @@ TELEGRAM_POST_URL.searchParams.append("parse_mode", "HTML");
 //     return true;
 // }
 
-//TEST CODE, to be deleted
-let full_telegram_url = new URL(TELEGRAM_POST_URL.toString());
-full_telegram_url.searchParams.append("text", new Date().toString());
-fetch(full_telegram_url, {method: 'POST'});
+// TEST CODE, to see if deployment works on remote server
+// let full_telegram_url = new URL(TELEGRAM_POST_URL.toString());
+// full_telegram_url.searchParams.append("text", new Date().toString());
+// fetch(full_telegram_url, {method: 'POST'});
 
 app.listen(process.env.PORT);
